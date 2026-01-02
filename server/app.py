65db674ee77373cfe41fa5e99e4cfcc7e47a6fd0
Liban Golo <liban.golo@student.moringaschool.com>
2026-01-02 15:30:35 +0300

    restful lab done

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -11,41 +10,62 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-migrate = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
 
 api = Api(app)
 
 
-class Plants(Resource):
-
+# Index route
+class Index(Resource):
     def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return {"message": "Welcome to the Plant Store API"}, 200
+
+
+api.add_resource(Index, '/')
+
+
+# All plants
+class Plants(Resource):
+    def get(self):
+        plants = [p.to_dict() for p in Plant.query.all()]
+        return plants, 200
 
     def post(self):
         data = request.get_json()
-
         new_plant = Plant(
             name=data['name'],
             image=data['image'],
             price=data['price'],
+            is_in_stock=data.get('is_in_stock', True)
         )
-
         db.session.add(new_plant)
         db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
+        return new_plant.to_dict(), 201
 
 
 api.add_resource(Plants, '/plants')
 
 
+# Single plant
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get_or_404(id)
+        return plant.to_dict(), 200
+
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id)
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(plant, key, value)
+        db.session.commit()
+        return plant.to_dict(), 200
+
+    def delete(self, id):
+        plant = Plant.query.get_or_404(id)
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
